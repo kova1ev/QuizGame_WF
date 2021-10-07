a2f9ddb9341bb2aba740b1fb3e1db0db9f1ad590
Andrey <kov92av@gmail.com>
2021-10-07 22:10:40 +0300

    Add class User

---
+++ QuizGame.Data/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Data.Model
+{
+    public class User
+    {
+        public int Score { get; set; }
+        public List<int> IdList { get; set; }
+    }
+}
